413650ed89fb4e33c31ab4186cbf42a7c8e38bf5
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2023-08-21 05:27:56 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,5 +1,14 @@
+from langchain.llms import GooglePalm
+import os
+from langchain.prompts import PromptTemplate
+
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
 import streamlit as st
 from langchain_helper import generate_game_name_and_functions
+
+
 
 # Set page title and favicon
 st.set_page_config(page_title="Game Idea Generator", page_icon="🎮")
@@ -51,6 +60,39 @@ st.markdown('<div class="container">', unsafe_allow_html=True)
 st.markdown('<div class="content">', unsafe_allow_html=True)
 type = st.text_input("🎮 Enter a Game Type:", "")
 st.markdown('</div>', unsafe_allow_html=True)
+
+
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyD29fEos3V6S2L-AGSQgNu03GqZEIgJads"
+
+
+llm = GooglePalm(temperature = 0.9)
+
+
+
+def generate_game_name_and_functions(type):
+
+    prompt_template_name = PromptTemplate(
+       input_variables = ['type'],
+       template = "I want to buid a new never build before {type} game, Suggest only one fancy and creative name"
+    )
+    name_chain = LLMChain(llm=llm, prompt = prompt_template_name, output_key = "game_name")
+    
+    prompt_template_items = PromptTemplate(
+        input_variables = ['game_name'],
+        template = "Write a short creative user manual for {game_name} game. Tell shortly in bullet points"
+    )
+
+    function_chain = LLMChain(llm=llm, prompt = prompt_template_items, output_key = 'functions')
+
+    chain= SequentialChain(chains= [name_chain, function_chain], 
+                       input_variables = ["type"], 
+                       output_variables = ["game_name","functions"])
+
+    response = chain({'type':type})
+
+    return response
+
 
 if type:
     response = generate_game_name_and_functions(type)
